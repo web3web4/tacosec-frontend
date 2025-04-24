4061be474b9725c5601cfacfc99ade7157657623
fade sarakpe <fadesarakpe12345@gmail.com>
2025-04-24 14:34:16 +0300

    Fix Error

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -14,7 +14,6 @@ const UserContext = createContext<UserContextType | null>(null);
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [userData, setUserData] = useState<initDataType | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [isReady, setIsReady] = useState(false); 
 
   const signUserData = async () => {
     setError(null);
@@ -23,14 +22,11 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
         setError("Telegram WebApp is not supported");
         return;
       }
-
+      
       const tg = window.Telegram.WebApp;
       tg.ready();
+      tg.expand();
       const initData = tg.initData;
-      if (!initData) {
-        throw new Error("Telegram WebApp data is missing");
-      }
-
       const response = await signupUser(initData);
       setUserData(response);
     } catch (err) {
@@ -39,39 +35,12 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
         icon: "error",
         title: "Error",
         text: error!,
-      });
-    }
+      });    }
   };
 
   useEffect(() => {
-    const checkWebAppReady = () => {
-      if (typeof window !== "undefined" && window.Telegram?.WebApp) {
-        const tg = window.Telegram.WebApp;
-        if (tg && tg.initData) {
-          setIsReady(true);
-        }
-      }
-    };
-
-    const intervalId = setInterval(() => {
-      checkWebAppReady();
-    }, 1000);
-
-    const timeoutId = setTimeout(() => {
-      clearInterval(intervalId);
-      setError("WebApp took too long to load");
-    }, 100000);
-
-    if (isReady) {
-      signUserData();
-      clearInterval(intervalId);
-    }
-
-    return () => {
-      clearInterval(intervalId);
-      clearTimeout(timeoutId);
-    };
-  }, [isReady]);
+    signUserData();
+  },[]);
 
   const value = {
     userData,
@@ -92,4 +61,4 @@ export function useUser() {
     throw new Error('useUser must be used within a UserProvider');
   }
   return context;
-}
+} 
