cc72c609e38e531793772db25ac24723127ad81c
fade sarakpe <fadesarakpe12345@gmail.com>
2025-04-24 12:11:01 +0300

    Edit Type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,12 @@
 export interface initDataType {
-  user?: {
-    id?: number;
-    username?: string;
-    first_name?: string;
-    last_name?: string;
-    auth_date?: string;
-    hash?: string;
-  };
+  _id: string,
+    telegramId: string,
+    firstName: string,
+    lastName: string,
+    username: string,
+    authDate: string,
+    hash: string,
+    isActive: boolean,
+    createdAt: string,
+    updatedAt: string,
 }
